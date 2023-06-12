c3b0c5688c949fb80dbdc9c4942e2049ef0d691c
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-06-11 21:19:31 -0400

    refactor: Move death listener to separate script and rewrite logic
    This commit separates the death listener functionality into its own script for improved code organization and maintainability. The logic for handling death messages has also been rewritten for clarity and extensibility.
    
    Changes in this commit:
    - Moved the `setupDeathListener` function to its own file (`deathMessage.js`).
    - Rewrote the logic for extracting death reasons and sending death messages.
    - Added type annotations to function parameters for improved type safety.
    
    These changes simplify the complexity of the main script and make the death listener code more modular and reusable.

---
+++ src/death_listener/deathMessage.ts
@@ -0,0 +1,99 @@
+import { EmbedBuilder, TextBasedChannel } from "discord.js";
+import config from "../config.js";
+import { Client } from "bedrock-protocol";
+
+export function setupDeathListener(bot: Client, channelId: TextBasedChannel) {
+    bot.on("text", (packet) => {
+        if (packet.message.includes("death")) {
+            let playername = packet.parameters[0];
+
+            if (playername.includes("%entity")) {
+                playername = "A tamed Animal";
+            }
+
+            let reason = getDeathReason(packet.message, packet.parameters[1]);
+
+            sendDeathMessage(channelId, playername, reason);
+        }
+    });
+}
+
+function getDeathReason(message: string, parameter: string): string {
+    const deathReasons: { [key: string]: string } = {
+        "death.attack.mob": getMobDeathReason(parameter),
+        "death.attack.arrow": "was killed by a Skeleton's Arrow.",
+        "death.attack.indirectMagic": "was killed by indirect magic.",
+        "death.attack.bullet": "was killed by a bullet.",
+        "death.attack.inWall": "Suffocated to death!",
+        "death.attack.explosion.player": "Was Blown to bits by an explosion",
+        "death.attack.onFire": "Went up in flames!",
+        "death.attack.player": "Was killed by " + parameter,
+        "death.attack.inFire": "tried to take a bath in lava.",
+        "death.attack.drown": parameter + " drowned to death!",
+        "death.attack.outOfWorld": "Fell into the void.",
+        "death.attack.sonicBoom.player": "Was blasted by the wardens sonic boom.",
+        "death.fell.accident.generic": "Fell to their death.",
+        "death.attack.fall": "Fell to their death.",
+        "death.attack.player.item": "Was killed by " + parameter,
+        "death.attack.lava": "Was playing with lava and died.",
+        default: "General Death - " + message + " - " + parameter,
+    };
+
+    return deathReasons[message] || deathReasons.default;
+}
+
+function getMobDeathReason(entity: string): string {
+    const mobDeathReasons: { [key: string]: string } = {
+        "%entity.zombie.name": "was killed by a Zombie.",
+        "%entity.skeleton.name": "was killed by a Skeleton's Arrow.",
+        "%entity.spider.name": "was killed by a Spider.",
+        "%entity.enderman.name": "was killed by an Enderman.",
+        "%entity.zombie_pigman.name": "was killed by a Zombie Pigman.",
+        "%entity.iron_golem.name": "was killed by an Iron Golem.",
+        "%entity.piglin_brute.name": "was killed by a Piglin Brute.",
+        "%entity.piglin.name": "was killed by a Piglin.",
+        "%entity.wither_skeleton.name": "was killed by a Wither Skeleton.",
+        "%entity.bee.name": "was chased by bees and got stung to death.",
+        "%entity.magma_cube.name": "was killed by a Magma Cube.",
+        "%entity.zoglin.name": "was killed by a Zoglin.",
+        "%entity.blaze.name": "was killed by a Blaze.",
+        "%entity.polar_bear.name": "was killed by a Polar Bear.",
+        "%entity.wolf.name": "was killed by a pack of wolves.",
+        "%entity.guardian.name": "was killed by a Guardian.",
+        "%entity.elder_guardian.name": "was killed by an Elder Guardian.",
+        "%entity.stray.name": "was killed by a Stray.",
+        "%entity.husk.name": "was killed by a Husk.",
+        "%entity.pillager.name": "was killed by a Pillager.",
+        "%entity.vex.name": "was killed by a Vex.",
+        "%entity.evocation_illager.name": "was killed by an Evoker's magic powers.",
+        "%entity.vindicator.name": "was killed by a Vindicator.",
+        "%entity.shulker.name": "was shot by a Shulker.",
+        "%entity.ender_dragon.name": "was killed by the Ender Dragon.",
+        "%entity.witch.name": "was killed by a Witch.",
+        "%entity.warden.name": "got flattened by the Warden.",
+        "%entity.drowned.name": "was killed by a Drowned",
+        default: "was killed by a mob - " + entity,
+    };
+
+    return mobDeathReasons[entity] || mobDeathReasons.default;
+}
+
+function sendDeathMessage(channelId: TextBasedChannel, playername: string, reason: string) {
+    if (config.useEmbed === true) {
+        const msgEmbed = new EmbedBuilder()
+            .setColor(config.setColor)
+            .setTitle(config.setTitle)
+            .setDescription("[In Game] " + playername + ": " + reason);
+        if (typeof channelId === "object" && typeof channelId.send === "function") {
+            channelId.send({ embeds: [msgEmbed] });
+        } else {
+            console.log("I could not find the in-game channel in Discord.");
+        }
+    } else {
+        if (typeof channelId === "object" && typeof channelId.send === "function") {
+            channelId.send(`[In Game] **${playername}**: ${reason}`);
+        } else {
+            console.log("I could not find the in-game channel in Discord.");
+        }
+    }
+}
