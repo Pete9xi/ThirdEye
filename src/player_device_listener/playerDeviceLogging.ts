8690e95bcd8a35566b4392a019613dde91fd2a39
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-06-12 22:02:33 -0400

    Move code from main.ts to playerDeviceLogging.ts and invoke it in main.ts for player device logging
    This commit moves the code responsible for player device logging from main.ts to a separate file named playerDeviceLogging.ts. The code is encapsulated in the addPlayerListener function. In main.ts, the addPlayerListener function is invoked to listen for player events and perform device logging accordingly. This refactoring improves code organization and separates concerns for better maintainability.

---
+++ src/player_device_listener/playerDeviceLogging.ts
@@ -0,0 +1,63 @@
+import { MessagePayload, MessageCreateOptions } from "discord.js";
+import { EmbedBuilder, TextBasedChannel } from "discord.js";
+import config from "../config.js";
+import { Client } from "bedrock-protocol";
+
+export function addPlayerListener(bot: Client, channelId: TextBasedChannel, WhitelistRead: any) {
+    const Whitelist = WhitelistRead.whitelist;
+
+    bot.on("add_player", (packet) => {
+        const deviceOS = getDeviceName(packet.device_os);
+        const gameDescription = `[In Game] ${packet.username}: Has joined the server using ${deviceOS}`;
+        let description = gameDescription;
+
+        if (config.blacklistDeviceTypes.includes(packet.device_os) && !Whitelist.includes(packet.username)) {
+            const cmd = `/kick ${packet.username} device is blacklisted.`;
+            bot.queue("command_request", {
+                command: cmd,
+                origin: {
+                    type: "player",
+                    uuid: "",
+                    request_id: "",
+                },
+                internal: false,
+                version: 52,
+            });
+
+            description = `[Server] ${packet.username}: Has been kicked as the device has been blacklisted: ${packet.device_os}`;
+        }
+
+        if (config.useEmbed === true) {
+            const msgEmbed = new EmbedBuilder().setColor(config.setColor).setTitle(config.setTitle).setDescription(description);
+            sendToChannel(channelId, { embeds: [msgEmbed] }, "I could not find the in-game channel in Discord. 2");
+        } else {
+            sendToChannel(channelId, description, "I could not find the in-game channel in Discord. 3");
+        }
+    });
+}
+
+function sendToChannel(channelId: TextBasedChannel, content: string | MessagePayload | MessageCreateOptions, errorMessage: string) {
+    if (typeof channelId === "object") {
+        channelId.send(content);
+    } else {
+        console.log(errorMessage);
+    }
+}
+
+function getDeviceName(deviceOS: string): string {
+    switch (deviceOS) {
+        case "Win10":
+            return "Windows PC";
+        case "IOS":
+            return "Apple Device";
+        case "Nintendo":
+            return "Nintendo Switch";
+        case "Android":
+            return "Android";
+        case "Orbis":
+            return "PlayStation";
+        default:
+            console.log("DeviceOS defaulted to packet.device_os");
+            return deviceOS;
+    }
+}
