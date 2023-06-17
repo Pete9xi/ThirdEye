00692176bc72a0c7bce2ff92571f0e5621eb9aa9
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-06-17 14:49:52 -0400

    Fix system command logging and update types

--- src/types/text/index.d.ts
+++ src/types/text/index.d.ts
@@ -1,5 +1,9 @@
 type RawText = {
     text: string;
+    translate?: string;
+    with?: {
+        rawtext: RawText[];
+    };
 };
 
 type ChatPacket = {
@@ -14,6 +18,16 @@ type ChatPacket = {
 
 type WhisperPacket = {
     type: "json_whisper";
+    needs_translation: boolean;
+    source_name: undefined | string;
+    message: string;
+    parameters: undefined | any;
+    xuid: string;
+    platform_chat_id: string;
+};
+
+type JsonPacket = {
+    type: "json";
     needs_translation: boolean;
     source_name: undefined | string;
     message: string;
